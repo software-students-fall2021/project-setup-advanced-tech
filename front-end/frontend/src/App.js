526d46d3948a8f04cdab70b1dfd579de26e899bf
dakhtar1 <akhtardaanyal@gmail.com>
2021-10-21 22:44:03 -0400

    first commit

--- front-end/frontend/src/App.js
+++ front-end/frontend/src/App.js
@@ -8,7 +8,9 @@ function App() {
         <h1>Welcome to Weet.</h1>
         <h2><i>A place for all your dietary needs.</i></h2>
       </header>
-      <button><a href="https://www.google.com/">Click to explore.</a></button>
+      <div class="center-button">
+        <button><a href="https://www.google.com/">Click to explore.</a></button>
+      </div>
     </div>
   );
 }
