bde9be26d7831d74ae83b2fd715edaeb39d740b7
dakhtar1 <akhtardaanyal@gmail.com>
2021-10-24 00:06:10 -0400

    Changes

--- front-end/frontend/src/accountSignIn/AccountSignIn.js
+++ front-end/frontend/src/accountSignIn/AccountSignIn.js
@@ -3,29 +3,15 @@ import PropTypes from 'prop-types';
 import styles from './AccountSignIn.module.css';
 import '../button.css';
 import WelcomeHeader from '../welcomeHeader/WelcomeHeader';
-import SignIn from '../signIn/SignIn';
+import SignIn from '../signin/SignIn';
 import {Route, Link} from 'react-router-dom';
 
 const AccountSignIn = () => (
   <div className={styles.buttons}>
-    <div class={styles.spacing}></div>
-    <div class={styles.spacing}></div>
-    <div class={styles.spacing}></div>
-    <div class={styles.spacing}></div>
-    <Route path="/login" component={SignIn}></Route>
-    <Link to="/login"><button className="welcomeButton">Sign in</button></Link>
-    <div class={styles.spacing}></div>
-    <div class={styles.spacing}></div>
-    <div class={styles.spacing}></div>
-    <button className="welcomeButton">Create Account</button>
-    <div class={styles.spacing}></div>
-    <div class={styles.spacing}></div>
-    <div class={styles.spacing}></div>
-    <button className="welcomeButton">Reset Password</button>
-    <div class={styles.spacing}></div>
-    <div class={styles.spacing}></div>
-    <div class={styles.spacing}></div>
-    <button className="welcomeButton">Contact Us</button>
+    <button className="welcomeButton" id={styles.first}>Sign in</button>
+    <button className="welcomeButton" id={styles.second}>Create Account</button>
+    <button className="welcomeButton" id={styles.third}>Reset Password</button>
+    <button className="welcomeButton" id={styles.fourth}>Contact Us</button>
   </div>
 );
 
--- front-end/frontend/src/signin/SignIn.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SignIn from './SignIn';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SignIn />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
