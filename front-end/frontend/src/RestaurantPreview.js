c302f0e1fe49ba8f2bbbde096a96b6fdac160be2
pl2285@nyu.edu <leo20021210@163.com>
2021-10-27 02:00:20 -0400

    21/10/27

---
+++ front-end/frontend/src/RestaurantPreview.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { Jumbotron } from 'reactstrap';
+import {Image} from 'react-bootstrap'
+import {Row} from 'react-bootstrap'
+import {Col} from 'react-bootstrap'
+import './Dishes.css'
+import {Button} from 'react-bootstrap'
+
+const RestaurantPreview = (props) => {
+  // console.log(props);
+
+  // inject a random placeholder image from the Lorem Picsum API
+  // the mockaroo API we're using doesn't include this
+  // ultimately, this data would come from the API
+  const imgSrc = `https://picsum.photos/100?id=${props.details.id}` // tack on this animal's id to the query
+
+  return (
+    /*<article className="Dishes">
+      <img alt={props.details.title} src={imgSrc} />
+      <h2>{props.details.title}</h2>
+    </article>*/
+    <Container className="restaurant">
+    <Jumbotron className="box">
+    <Link to={`/retaurant/${props.details.id}`}>
+      <Col className="image" xs={3} md={3}>
+        <Image src={imgSrc} thumbnail/>
+      </Col>
+      <Col className="word" xs={3} md={3}>
+        <Row>
+          <Col xs={5}>
+            <h3>{props.details.name}</h3>
+          </Col>
+          <Col>
+            <Button><span>{props.details.collection}</span><span>Collect</span></Button>
+          </Col>
+        </Row>
+        <Row>
+          <p>{props.details.city},{props.details.state}</p>
+          <p>{props.details.dishnum} dishes in total</p>
+        </Row>
+      </Col>
+    </Link>
+    </Jumbotron>
+    </Container>
+  )
+}
+
+// make this function available to be imported into another module
+export default RestaurantPreview
