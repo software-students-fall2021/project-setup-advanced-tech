33ed5ae83e2f19f76a84d3834d9e59ffbc27d8ac
neevmittal <neevmittal@gmail.com>
2021-10-26 23:29:16 -0400

    merge

--- front-end/frontend/src/DishOrder/OrderPage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import OrderPage from './OrderPage';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<OrderPage />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
