128d61559558a227163482406101191054665bc4
dakhtar1 <akhtardaanyal@gmail.com>
2021-11-07 21:36:43 -0500

    Front end changes

--- front-end/frontend/src/accountSignIn/AccountSignIn.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AccountSignIn from './AccountSignIn';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AccountSignIn />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
