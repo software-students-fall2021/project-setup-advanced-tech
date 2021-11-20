029ac8a2f79b980451d84f00e2f290ffe728b41a
pl2285@nyu.edu <leo20021210@163.com>
2021-11-20 16:22:07 -0500

    2021/11/20

--- back-end/App.js
+++ back-end/App.js
@@ -212,3 +212,5 @@ app.post("/restaurants", (req, res, next) => {
 
 //passport.authenticate("jwt", {successRedirect: '/restaurants', failureRedirect: '/login', failureFlash: true}),
 
+const dishesRouter = require('./Router/Dishes/Dishes')
+app.use('/dishes', dishesRouter)
--- front-end/frontend/src/App.js
+++ front-end/frontend/src/App.js
@@ -98,6 +98,8 @@ function App() {
 
   return (
     <div className="App">
+      <RestaurantList></RestaurantList>
+      <Restaurant id = "5"></Restaurant>
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
