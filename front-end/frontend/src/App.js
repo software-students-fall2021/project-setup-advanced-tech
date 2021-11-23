557c352db6d50592d527a089485791f4398d19a0
dakhtar1 <akhtardaanyal@gmail.com>
2021-11-22 23:17:00 -0500

    Needed to go dto master because of conflict

--- front-end/frontend/src/App.js
+++ front-end/frontend/src/App.js
@@ -100,8 +100,6 @@ function App() {
 
   return (
     <div className="App">
-      <RestaurantList></RestaurantList>
-      <Restaurant id = "5"></Restaurant>
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
